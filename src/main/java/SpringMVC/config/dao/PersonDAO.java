12089173aa18ab643c768e0a1077d5dea920a249
Kirill Kolpakov <kirill.48@inbox.ru>
2024-02-01 16:54:56 +0300

    Start project

---
+++ src/main/java/SpringMVC/config/dao/PersonDAO.java
@@ -0,0 +1,53 @@
+package SpringMVC.config.dao;
+
+import SpringMVC.config.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNT;
+
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT,"Jessy"));
+        people.add(new Person(++PEOPLE_COUNT,"Jerry"));
+        people.add(new Person(++PEOPLE_COUNT,"Sam"));
+        people.add(new Person(++PEOPLE_COUNT,"Bob"));
+
+    }
+
+    public List<Person> index()
+    {
+        return people;
+    }
+
+    public Person show(int id)
+    {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person)
+    {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson)
+    {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id)
+    {
+        people.removeIf(person -> person.getId() == id);
+    }
+
+}
